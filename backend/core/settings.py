293bad0baf192c828bec2392a7ffc6c05f3fa78a
Paulo <paulovitor.lavinas@gmail.com>
2026-01-28 14:54:52 +0000

    chore: migrate environment to PostgreSQL changing in settings.py

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mediatracker_db',
+        'USER': 'mediatracker_user',
+        'PASSWORD': 'mediatracker_pass',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
