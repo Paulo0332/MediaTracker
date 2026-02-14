bec48b8b9d70eb6f9827e6ecf093693f9c396e3c
Paulo <paulovitor.lavinas@gmail.com>
2026-02-14 13:58:59 +0000

    feat(admin): finalized admin engine with inlines tables and optimized lookup through raw_id_fields

--- backend/media/admin.py
+++ backend/media/admin.py
@@ -4,6 +4,6 @@ from media.models import Media
 # Register your models here.
 @admin.register(Media)
 class MediaAdmin(admin.ModelAdmin):
-    list_display = ("title","creator","media_type","release_date")
+    list_display = ("id","title","creator","media_type","release_date")
     list_filter = ("media_type",)
     search_fields = ("title","creator")
